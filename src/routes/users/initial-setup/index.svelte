495d4fcc2b89cc4c9a374a6b9464d02240823cdf
Bob McAleavey <bobmcaleavey@gmail.com>
2021-09-14 08:40:32 -0400

    add route users/initial-setup

---
+++ src/routes/users/initial-setup/index.svelte
@@ -0,0 +1,25 @@
+<script>
+  // components
+  import { Datatable, Section } from '$components';
+  
+  // props ( internal )
+  const columns = [
+    {title:'First Name', key:'firstName'},
+    {title:'Last Name', key:'lastName'},
+    {title:'Username', key:'username'},
+    {title:'Initial Password', key:'initialPassword'}
+  ]
+  const datatable = {
+    collection: 'users',
+    columns : [...columns].filter(column=>column.key!=='roles'),
+    deleteModalFN : row => `"${row.firstName} ${row.lastName}"`,
+    editable: false,
+    sort : {
+      firstName: 1,
+    }
+  }
+</script>
+
+<Section class="space-y-[16px]">
+  <Datatable {...datatable} />
+</Section>
