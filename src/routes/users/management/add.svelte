0b3da85df8c645da6afbb635856c2890fce1575d
Bob McAleavey <bobmcaleavey@gmail.com>
2021-09-21 14:08:03 -0400

    refactor for hire date

--- src/routes/users/management/add.svelte
+++ src/routes/users/management/add.svelte
@@ -3,6 +3,7 @@
   import { serverFetch } from '$lib/_helpers.js';
   import { onMount } from 'svelte'
   import columns from './_columns';
+  import moment from 'moment'
   
   // components
   import { Button, Buttons, Card, Input, Section, Select, Spinner } from '$components';
@@ -14,13 +15,11 @@
   }
   const submitHandler = async () => {
     modal.spinner.show();
-    let user = {
-      firstName: body.firstName.value,
-      lastName: body.lastName.value,
-      username: body.username.value,
-      extension: body.extension.value,
-      roles: body.roles.value,
-    }
+    const user = Object.keys(body).reduce((obj, key)=>{
+      obj[key] = body[key].value;
+      if ( body[key]?.type === 'date' ) obj[key] = moment(obj[key], 'MM.DD.YYYY').format('x')
+      return obj;
+    }, {})
     const data = await serverFetch({method:'POST', href:'/api/auth/signup', body: user})
     const { accessToken, password } = data;
     const modalInnerHTML = `Successfully added user "${body.firstName.value} ${body.lastName.value}".  Copy text below to have user verify account.<br><br>Please verify your account with the link below with password <span class="font-bold">${password}</span>:<br><a href="${import.meta.env.VITE_BASE_URL}/users/verify?accessToken=${accessToken}">Verify</a>`
@@ -58,10 +57,14 @@
   {#if loaded}
     <form on:submit|preventDefault={submitHandler} class="flex flex-col mt-[24px] space-y-[16px] max-w-[500px] w-full mx-auto">
       {#each Object.keys(body) as key}
-        {#if !('type' in body[key])}
-          <Input label={body[key].label} placeholder={body[key].label} bind:value={body[key].value} />
+        {#if ('type' in body[key])}
+          {#if body[key].type === 'date'}
+            <Input label={body[key].label} placeholder={body[key].label} bind:value={body[key].value} type="date" />
+          {:else}
+            <Select label={body[key].label} bind:value={body[key].value} multiple={body[key].multiple} placeholder={body[key].placeholder} options={selects[key]} />
+          {/if}
         {:else}
-          <Select label={body[key].label} bind:value={body[key].value} multiple={body[key].multiple} placeholder={body[key].placeholder} options={selects[key]} />
+          <Input label={body[key].label} placeholder={body[key].label} bind:value={body[key].value} />
         {/if}
       {/each}
       <Button type="submit">Add</Button>
