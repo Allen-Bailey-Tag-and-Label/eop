d9db9e5876f96a86d340a0069ea3be88d1e4fe78
Bob McAleavey <bobmcaleavey@gmail.com>
2021-09-23 09:17:28 -0400

    add width prop

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,11 +1,13 @@
-<style>
-  table tr:hover td {
-    background-color: rgba(255,255,255,.05);
-  }
-</style>
+<script>
+  // props ( external )
+  export let width = "w-full"
+  
+  // props ( dynamic )
+  $: classes = `${width}`;
+</script>
 
 <div class="overflow-auto w-full scrollbar-thin scrollbar-track-transparent scrollbar-thumb-primary-500 scrollbar-track-rounded-full">
-  <table class="w-full">
+  <table class={classes}>
     <slot name="thead">
       <thead></thead>
     </slot>
