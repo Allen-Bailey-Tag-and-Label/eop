25289fbc0a23c19d97c3457ff9178cf5b544dd9f
Bob McAleavey <bobmcaleavey@gmail.com>
2021-10-25 10:33:41 -0400

    fix - change modal message to display html

--- src/components/Modal/Confirmation/Modal.svelte
+++ src/components/Modal/Confirmation/Modal.svelte
@@ -21,7 +21,7 @@
     <Card class="max-w-[375px] transition duration-200 {$store.show?'opacity-100 pointer-events-auto':'opacity-0 pointer-events-none'}">
       <div class="flex flex-col items-center space-y-[32px]">
         <Icon src={Exclamation} class="w-[80px] h-[80px] text-red-500" />
-        <div>{$store.message}</div>
+        <div>{@html $store.message}</div>
         <div class="flex space-x-[16px]">
           <Button theme="error" on:click={()=>store.hide()}>Cancel</Button>
           <Button on:click={confirmHandler}>Confirm</Button>
