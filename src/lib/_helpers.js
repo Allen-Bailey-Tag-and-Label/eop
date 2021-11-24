d5f89b108c22c2206781d8f4e5faddd2cec89fa7
Bob McAleavey <bobmcaleavey@gmail.com>
2021-11-24 13:00:01 -0500

    add - responseType logic

--- src/lib/_helpers.js
+++ src/lib/_helpers.js
@@ -40,12 +40,13 @@ export const set = (obj, path, value) => {
   return obj; // Return the top-level object to allow chaining
 };
 export const serverFetch = async (params) => {
-  let body, headers, href, method;
+  let body, headers, href, method, responseType;
 
   // check if string supplied
   if (typeof params === 'string') {
     href = params;
     method = 'GET';
+    responseType = 'JSON'
   } else {
     body = params.body;
     href = params.href;
@@ -53,8 +54,10 @@ export const serverFetch = async (params) => {
     headers = {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
-    }
+    };
+    responseType = 'JSON';
     if ('headers' in params) headers = params.headers;
+    if ('responseType' in params) responseType = params.responseType;
   }
 
   let options = { method }
@@ -65,6 +68,6 @@ export const serverFetch = async (params) => {
   }
 
   const response = await fetch(href, options);
-  const data = await headers?.['Content-Type'] === 'application/json' || headers === undefined ? response.json() : response.text();
+  const data = await (responseType === 'JSON' ? response.json() : response.text())
   return data;
 }
