295308375901ec0315e2ac138811a576a599d211
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-12 10:32:57 -0500

    [ADD] colors abtl-red abtl-blue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/sveltewind/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'abtl-red': '#C10F2A',
+				'abtl-blue': '#1C2B4F'
+			}
+		}
 	},
 
 	plugins: []
