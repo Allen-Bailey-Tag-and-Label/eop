4daa83ad9129c3e41991e4db7e3016026a8b1dd7
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-12 10:34:32 -0500

    [ADD] route signin

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,38 @@
 <script>
+	// imports
+	import { onMount } from 'svelte';
+	import { theme } from 'sveltewind/stores';
+	import { Main, Overlay, Pwa, SafeArea } from '@components';
+	import { twMerge } from 'tailwind-merge';
 	import '../app.css';
+
+	// props (internal)
+	let loaded = false;
+
+	// props (dynamic)
+	$: overlayClasses = loaded ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto';
+
+	// lifecycle
+	onMount(() => {
+		theme.set({
+			fieldset: 'space-y-[.25rem]',
+			input: 'dark:bg-gray-900 dark:text-gray-50 dark:ring-offset-gray-700'
+		});
+		loaded = true;
+	});
 </script>
 
-<slot />
+<Pwa />
+
+<Main>
+	<SafeArea>
+		<slot />
+	</SafeArea>
+</Main>
+
+<Overlay
+	class={twMerge(
+		'transition duration-200 h-screen absolute bg-gray-50 dark:bg-gray-900 flex-grow',
+		overlayClasses
+	)}
+/>
---
+++ src/routes/signin/index.svelte
@@ -0,0 +1,27 @@
+<script>
+	// imports
+	import { Button, Fieldset, Form, H3, Input, Logo } from '@components';
+
+	// handlers
+	const submitHandler = (e) => {
+		e.preventDefault();
+	};
+
+	// props (internal)
+	let password = '';
+	let username = '';
+</script>
+
+<div class="flex flex-col flex-grow items-center justify-center p-[1.5rem]">
+	<Form class="w-full max-w-sm" on:submit={submitHandler}>
+		<Logo class="mx-auto" />
+		<H3 class="text-center">Signin</H3>
+		<Fieldset legend="Username">
+			<Input bind:value={username} />
+		</Fieldset>
+		<Fieldset legend="Password">
+			<Input bind:value={password} type="password" />
+		</Fieldset>
+		<Button type="submit">Signin</Button>
+	</Form>
+</div>
