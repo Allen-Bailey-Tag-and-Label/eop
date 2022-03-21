8cf349ae56281fff4f4a1b18801757f3eadcd64b
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-21 12:21:45 -0400

    [FIX] add fallback port 3000 if none specified in ENV variables

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 		adapter: adapter({
 			entryPoint: ['./server.js'],
 			env: {
-				port: process.env.PORT | 3000,
+				port: process.env.PORT || 3000,
 				host: '0.0.0.0'
 			}
 		}),
