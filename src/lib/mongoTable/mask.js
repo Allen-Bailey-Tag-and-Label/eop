f5545ea6cce54e177f7fd6d5955a8e1538b2b26c
Bob McAleavey <bobmcaleavey@gmail.com>
2022-10-10 13:41:26 -0400

    fix: add 1 to mask date

--- src/lib/mongoTable/mask.js
+++ src/lib/mongoTable/mask.js
@@ -1,7 +1,7 @@
 export default {
   date: (v) => {
     const d = new Date(v);
-    return `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
+    return `${d.getMonth() + 1}/${d.getDate() + 1}/${d.getFullYear()}`;
   },
   datetime: (v) => `${new Date(v).toLocaleDateString()} ${new Date(v).toLocaleTimeString()}`,
   extension: (v) => (v === 0 ? '' : v),
