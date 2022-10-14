d4ec06580ce1c985f8cddc5ad2cde5306d4c09d4
Bob McAleavey <bobmcaleavey@gmail.com>
2022-10-14 16:43:11 -0400

    improvement: minor css styling

--- src/lib/components/Range.svelte
+++ src/lib/components/Range.svelte
@@ -14,7 +14,7 @@
 <div class="relative">
   <input bind:value class={classes} {max} {min} type="range" />
   <div
-    class="absolute top-1/2 left-0 -translate-y-1/2 rounded-full h-[.5rem] bg-primary-500"
+    class="absolute top-1/2 left-0 -translate-y-1/2 rounded-full h-[.5rem] bg-primary-500/[.5]"
     style="width: {((value - min) / (max - min)) * 100}%"
   />
 </div>
