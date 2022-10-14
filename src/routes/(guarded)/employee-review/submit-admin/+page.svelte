3a387b8ebd9dd0bc2909a3e4cb6b1f617bfbe887
Bob McAleavey <bobmcaleavey@gmail.com>
2022-10-14 13:11:01 -0400

    feat: add /employee-review/submit-admin route

---
+++ src/routes/(guarded)/employee-review/submit-admin/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Route from '../submit/+page.svelte';
+
+  // props (external)
+  export let data;
+</script>
+
+<Route {data} title="Employee Review - Submit (Admin)" userFilterFn={() => true} />
