26f2869084d94aecfccf1f2bd9e5b6756508d40d
Bob McAleavey <bobmcaleavey@gmail.com>
2022-10-14 16:22:14 -0400

    feat: add Range component

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -23,4 +23,5 @@ export { default as Nav } from './Nav.svelte';
 export { default as NavGroup } from './NavGroup.svelte';
 export { default as NavLink } from './NavLink.svelte';
 export { default as Radio } from './Radio.svelte';
+export { default as Range } from './Range.svelte';
 export { default as TitleBar } from './TitleBar.svelte';
