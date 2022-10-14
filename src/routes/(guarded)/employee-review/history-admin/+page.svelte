daddb6450afe6a45ec3c35858c3e88619543f1c0
Bob McAleavey <bobmcaleavey@gmail.com>
2022-10-14 13:15:04 -0400

    feat: add /employee-review/history-admin route

---
+++ src/routes/(guarded)/employee-review/history-admin/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Route from '../history/+page.svelte';
+
+  // props (external)
+  export let data;
+</script>
+
+<Route {data} filters={[]} title="Employee Review - History (Admin)" />
