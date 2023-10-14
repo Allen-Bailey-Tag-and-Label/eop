4b6fde188d49adbd7cac92556138175b39fc3909
Bob McAleavey <bobmcaleavey@gmail.com>
2023-10-14 08:05:55 -0400

    feat: add logo

--- src/routes/(unathenticated)/+layout.svelte
+++ src/routes/(unathenticated)/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
-	import { Card } from '$components';
+	import { Card, Logo } from '$components';
 </script>
 
 <div class="flex flex-col flex-grow min-h-[100dvh] overflow-auto lg:grid lg:grid-cols-2">
-	<div class="hidden bg-primary-500 lg:flex" />
+	<div class="hidden bg-primary-500 lg:flex items-center justify-center">
+		<Logo class="w-[20rem]" fill1="fill-white" fill2="fill-white" />
+	</div>
 	<Card
 		class="flex flex-grow ring-0 p-0 rounded-none shadow-none dark:shadow-none items-center justify-center"
 	>
