188984826fa098365db6e5750e29de48ba85cbe8
Bob McAleavey <bobmcaleavey@gmail.com>
2023-12-08 08:51:32 -0500

    feat(paths): add paths and aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$actions: resolve('./src/actions'),
+			$components: resolve('./src/components'),
+			$icons: resolve('./src/icons'),
+			$stores: resolve('./src/stores'),
+			$transitions: resolve('./src/transitions')
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
