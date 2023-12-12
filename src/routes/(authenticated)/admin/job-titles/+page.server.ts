bb3f2d4000c9bb9bd556948eba14096de7cd1440
Bob McAleavey <bobmcaleavey@gmail.com>
2023-12-12 08:50:13 -0500

    fix(job-titles): correct model

--- src/routes/(authenticated)/admin/job-titles/+page.server.ts
+++ src/routes/(authenticated)/admin/job-titles/+page.server.ts
@@ -1,7 +1,7 @@
 import { prisma } from '$lib/prisma';
 import { createFormActions } from '$lib/dataTable';
 
-export const actions = createFormActions('Route');
+export const actions = createFormActions('JobTitle');
 export const load = async () => {
   const [jobTitles] = await Promise.all([
     prisma.jobTitle.findMany({
