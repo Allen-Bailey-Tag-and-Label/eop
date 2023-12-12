838d7ce88ad36e00da50801ef534c03da0070e12
Bob McAleavey <bobmcaleavey@gmail.com>
2023-12-12 14:13:10 -0500

    feat(nav): change css for navItem component

--- src/stores/theme/index.ts
+++ src/stores/theme/index.ts
@@ -21,7 +21,7 @@ theme.set({
   navGroup: 'flex flex-col',
   navGroupTitle: 'px-4 py-2 uppercase font-semibold lg:pl-[4rem]',
   navItem:
-    'pr-4 pl-8 py-2 shadow-[] opacity-50 hover:text-violet-500 focus:text-violet-500 hover:opacity-100 focus:opacity-100 transition duration-200 lg:pl-[5rem]',
+    'pr-4 pl-8 py-2 shadow-[] opacity-50 hover:opacity-100 focus:opacity-100 transition duration-200 lg:pl-[5rem]',
   navItemCurrent:
     'opacity-100 text-white dark:text-white bg-violet-500 hover:text-white focus:text-white cursor-default',
   responsiveTable: 'p-0 rounded-lg overflow-auto',
