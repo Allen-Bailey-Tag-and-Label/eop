e2853d06627062e7a5f9f69c063c64d24f8b88dc
Bob McAleavey <bobmcaleavey@gmail.com>
2024-03-09 08:29:44 -0500

    feat: reorder columns

--- src/routes/(authenticated)/(default)/users/user-management/+page.server.ts
+++ src/routes/(authenticated)/(default)/users/user-management/+page.server.ts
@@ -5,6 +5,7 @@ const { actions, load } = await getServerFunctions('User', {
 		['isActive', { label: 'Active' }],
 		['isOnboarded', { label: 'Onboarded' }]
 	]),
+	columnOrder: ['Username', 'Active', 'Onboarded', 'Roles'],
 	getRelationLabelFunctions: new Map([
 		['profile', (row) => `${row.firstName} ${row.lastName}`],
 		['roles', (row) => row.label]
--- src/routes/(authenticated)/(default)/users/user-profile-management/+page.server.ts
+++ src/routes/(authenticated)/(default)/users/user-profile-management/+page.server.ts
@@ -2,6 +2,16 @@ import { getServerFunctions } from '$lib/dbTable';
 
 const { actions, load } = await getServerFunctions('UserProfile', {
 	columns: new Map([]),
+	columnOrder: [
+		'First Name',
+		'Last Name',
+		'Ennis Id',
+		'Email',
+		'Extension',
+		'Date Hired',
+		'Email Signature Title',
+		'User'
+	],
 	getRelationLabelFunctions: new Map([['user', (row) => row.username]]),
 	orderBy: [{ firstName: 'asc' }, { lastName: 'asc' }]
 });
