7fbbf746f632062848879e1bd2b8f59350b4dc78
Bob McAleavey <bobmcaleavey@gmail.com>
2025-01-02 08:23:43 -0500

    fix: remove debugging from dashboard

--- src/routes/(authenticated)/(padded)/dashboard/+page.svelte
+++ src/routes/(authenticated)/(padded)/dashboard/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import { H1 } from 'sveltewind/components';
-	import * as Components from 'sveltewind/components';
+	import { H1 } from '$lib/components';
 </script>
 
 <H1>Dashboard</H1>
-<pre>{JSON.stringify(Object.keys(Components), null, 2)}</pre>
