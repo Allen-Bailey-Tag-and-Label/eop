46fcb5a2dcc5ec15edb3de3a4e04f65f04c0b234
Bob McAleavey <bobmcaleavey@gmail.com>
2025-01-04 08:00:55 -0500

    feat: add relationLabelFns

--- src/routes/(authenticated)/(padded)/crm/interactions/+page.server.ts
+++ src/routes/(authenticated)/(padded)/crm/interactions/+page.server.ts
@@ -1,5 +1,11 @@
 import { pageServer } from '$lib/prismaTable';
 
-const { actions, load } = await pageServer({ modelName: 'CRMInteraction' });
+const { actions, load } = await pageServer({
+	modelName: 'CRMInteraction',
+	relationLabelFns: new Map([
+		['customer', ({ label }) => label],
+		['userProfile', ({ firstName, lastName }) => `${firstName} ${lastName}`]
+	])
+});
 
 export { actions, load };
--- src/routes/(authenticated)/(padded)/customers/territories/+page.server.ts
+++ src/routes/(authenticated)/(padded)/customers/territories/+page.server.ts
@@ -1,5 +1,9 @@
 import { pageServer } from '$lib/prismaTable';
 
-const { actions, load } = await pageServer({ modelName: 'CustomerTerritory', sortKey: 'label' });
+const { actions, load } = await pageServer({
+	modelName: 'CustomerTerritory',
+	relationLabelFns: new Map([['createdBy', ({ username }) => username]]),
+	sortKey: 'label'
+});
 
 export { actions, load };
