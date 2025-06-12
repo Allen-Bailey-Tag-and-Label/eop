dc0224d1e7b97564646ac3e326115c60f0669111
Bob McAleavey <bobmcaleavey@gmail.com>
2025-06-12 15:55:29 -0400

    feat: add Div component

---
+++ src/lib/components/Div/Div.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = { children?: Snippet; class?: string };
+	let { children, class: className, ...restProps }: Props = $props();
+</script>
+
+<div class={className} {...restProps}>
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Button } from './Button/Button.svelte';
+export { default as Div } from './Div/Div.svelte';
