bea67760879bcbbe190be7b9a014830b62bd27ee
Bob McAleavey <bobmcaleavey@gmail.com>
2025-06-12 15:55:50 -0400

    feat: add Form component

---
+++ src/lib/components/Form/Form.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { twMerge } from 'tailwind-merge';
+	import { Div } from '../';
+
+	type Props = {
+		action?: string;
+		buttons?: Snippet;
+		children?: Snippet;
+		class?: string;
+		inputs?: Snippet;
+		method?: 'GET' | 'POST';
+	};
+	let {
+		action,
+		buttons,
+		children,
+		class: className,
+		inputs,
+		method = 'POST',
+		...restProps
+	}: Props = $props();
+</script>
+
+<form {action} class={twMerge('w-full max-w-sm space-y-12', className)} {method} {...restProps}>
+	{#if children}
+		{@render children()}
+	{/if}
+	{#if inputs}
+		<Div class="flex flex-col space-y-6">
+			{@render inputs()}
+		</Div>
+	{/if}
+	{#if buttons}
+		<Div class="flex flex-col gap-4 md:flex-row [&>*]:flex-grow">
+			{@render buttons()}
+		</Div>
+	{/if}
+</form>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './Button/Button.svelte';
 export { default as Div } from './Div/Div.svelte';
+export { default as Form } from './Form/Form.svelte';
