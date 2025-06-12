cfd88bd7775e72c4425d348dee9cf6afa40dc72b
Bob McAleavey <bobmcaleavey@gmail.com>
2025-06-12 15:57:14 -0400

    feat: add Label component

---
+++ src/lib/components/Label/Label.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { twMerge } from 'tailwind-merge';
+
+	type Props = { children?: Snippet; class?: string };
+	let { children, class: className, ...restProps }: Props = $props();
+</script>
+
+<label class={twMerge('font-semibold', className)} {...restProps}>
+	{#if children}
+		{@render children()}
+	{/if}
+</label>
