3b19a6fcbca35f2f71eb5f47fb3881eac3ffe9b3
Bob McAleavey <bobmcaleavey@gmail.com>
2025-06-12 15:57:54 -0400

    feat: add sign-in route

---
+++ src/routes/(public)/sign-in/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Button, Div, Form, Input } from '$lib/components';
+	import { ABTL, PTI } from '$lib/logos';
+	import { Plus } from '@lucide/svelte';
+</script>
+
+<Div class="grid grid-cols-1 md:grid-cols-2">
+	<Div class="flex min-h-screen flex-col items-center justify-center space-y-6 p-4">
+		<Div>
+			<Div class="flex items-center space-x-2 dark:hidden">
+				<ABTL size={128} />
+				<Plus size={36} />
+				<PTI size={128} />
+			</Div>
+			<Div class="hidden items-center space-x-2 dark:flex">
+				<ABTL color="currentColor" size={128} />
+				<Plus size={36} />
+				<PTI color="currentColor" size={128} />
+			</Div>
+		</Div>
+		<Form>
+			{#snippet inputs()}
+				<Input label="Username" required={true} />
+				<Input isPasswordButtonVisible={true} label="Password" required={true} type="password" />
+			{/snippet}
+			{#snippet buttons()}
+				<Button type="submit">Sign In</Button>
+			{/snippet}
+		</Form>
+	</Div>
+	<Div class="hidden min-h-screen items-center justify-center bg-slate-950/10 p-4 md:flex"></Div>
+</Div>
