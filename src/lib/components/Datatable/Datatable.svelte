9a9d20e4cea4bc2c99b8d9b50c14cc727089b0e3
bobthered <bobmcaleavey@gmail.com>
2025-06-18 12:00:42 -0400

    feat: add Datatable component

---
+++ src/lib/components/Datatable/Datatable.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+	import Table from '../Table/Table.svelte';
+	import Thead from '../Thead/Thead.svelte';
+	import Tr from '../Tr/Tr.svelte';
+	import Th from '../Th/Th.svelte';
+	import Input from '../Input/Input.svelte';
+	import Tbody from '../Tbody/Tbody.svelte';
+	import Td from '../Td/Td.svelte';
+
+    type Column = string;
+    type Props = { children?: Snippet; class?: string, columns:Column[]; rows: Row[]; style?: string };
+    type Row = Record<string, any>;
+
+    let { children, class: className, columns = $bindable(), rows = $bindable(), style, ...restProps }: Props = $props();
+</script>
+
+<Table>
+    <Thead>
+        <Tr>
+            <Th><Input type="checkbox" /></Th>
+            {#each columns as key}
+                <Th>{key}</Th>
+            {/each}
+        </Tr>
+    </Thead>
+    <Tbody>
+            {#each rows as row}
+                <Tr>
+                    <Td><Input type="checkbox" /></Td>
+                    {#each columns as key}
+                        <Td>{row[key] || ''}</Td>
+                    {/each}
+                </Tr>
+            {/each}
+    </Tbody>
+</Table>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,5 +1,6 @@
 export { default as A } from './A/A.svelte';
 export { default as Button } from './Button/Button.svelte';
+export { default as Datatable } from './Datatable/Datatable.svelte';
 export { default as Div } from './Div/Div.svelte';
 export { default as Form } from './Form/Form.svelte';
 export { default as Input } from './Input/Input.svelte';
