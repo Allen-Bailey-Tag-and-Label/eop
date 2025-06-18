4f00daa489fde651574f0a03a71880975bea7f6b
bobthered <bobmcaleavey@gmail.com>
2025-06-18 10:41:12 -0400

    feat: add Table component

---
+++ src/lib/components/Table/Table.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+
+    type Props = { children?: Snippet; class?: string, style?: string };
+    let { children, class: className, style, ...restProps }: Props = $props();
+</script>
+
+<table class={className} {style} {...restProps}>
+    {#if children}
+        {@render children()}
+    {/if}
+</table>
