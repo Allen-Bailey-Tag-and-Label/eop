54b0a894d5018e5bac55e628f2ab1a05c1bb3a2d
bobthered <bobmcaleavey@gmail.com>
2025-06-19 12:20:50 -0400

    feat: add Card component

---
+++ src/lib/components/Card/Card.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+	import { twMerge } from 'tailwind-merge';
+
+	type Props = { children?: Snippet; class?: string; style?: string };
+	let { children, class: className, style, ...restProps }: Props = $props();
+</script>
+
+<div
+	class={twMerge(
+		'rounded-sm bg-slate-50/30 p-6 backdrop-blur-md transition duration-200 dark:bg-slate-50/10',
+		className
+	)}
+	{style}
+	{...restProps}
+>
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
