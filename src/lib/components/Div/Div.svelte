6c1f9683ae301ab4cc1bae9eb1f9f17b51e85e39
bobthered <bobmcaleavey@gmail.com>
2025-06-24 10:48:54 -0400

    feat: add contenteditable & innerHTML props

--- src/lib/components/Div/Div.svelte
+++ src/lib/components/Div/Div.svelte
@@ -8,19 +8,43 @@
 		attachments?: Attachment[];
 		children?: Snippet;
 		class?: string;
+		contenteditable?: boolean | '';
+		innerHTML?: string;
 		style?: string;
 	} & any;
 	let {
 		attachments = $bindable([]),
 		children,
 		class: className,
+		contenteditable,
+		innerHTML = $bindable(),
 		style,
 		...restProps
 	}: Props = $props();
 </script>
 
-<div {...restProps} {@attach attachmentFactory(attachments)} class={twMerge('', className)} {style}>
-	{#if children}
-		{@render children()}
-	{/if}
-</div>
+{#if contenteditable === '' || contenteditable}
+	<div
+		{...restProps}
+		{@attach attachmentFactory(attachments)}
+		bind:innerHTML
+		class={twMerge('', className)}
+		contenteditable=""
+		{style}
+	>
+		{#if children}
+			{@render children()}
+		{/if}
+	</div>
+{:else}
+	<div
+		{...restProps}
+		{@attach attachmentFactory(attachments)}
+		class={twMerge('', className)}
+		{style}
+	>
+		{#if children}
+			{@render children()}
+		{/if}
+	</div>
+{/if}
