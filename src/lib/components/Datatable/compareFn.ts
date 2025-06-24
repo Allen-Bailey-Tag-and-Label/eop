ea4fbd90e8307d04af45acbc8878d115c565e0b6
bobthered <bobmcaleavey@gmail.com>
2025-06-24 10:48:24 -0400

    feat: add type guarding

--- src/lib/components/Datatable/compareFn.ts
+++ src/lib/components/Datatable/compareFn.ts
@@ -1,10 +1,12 @@
 export const compareFn = {
 	bigint: (a: bigint, b: bigint) => {
+		if (typeof a !== 'bigint' || typeof b !== 'bigint') return 0;
 		if (a === null) return 1;
 		if (b === null) return -1;
 		return BigInt('0');
 	},
 	boolean: (a: boolean, b: boolean) => {
+		if (typeof a !== 'boolean' || typeof b !== 'boolean') return 0;
 		if (a === null) return 1;
 		if (b === null) return -1;
 		if (a === b) return 0;
@@ -12,36 +14,43 @@ export const compareFn = {
 		return 1;
 	},
 	currency: (a: number, b: number) => {
+		if (typeof a !== 'number' || typeof b !== 'number') return 0;
 		if (a === null) return 1;
 		if (b === null) return -1;
 		return a - b;
 	},
 	function: (a: Function, b: Function) => {
+		if (typeof a !== 'function' || typeof b !== 'function') return 0;
 		if (a === null) return 1;
 		if (b === null) return -1;
 		return 0;
 	},
 	number: (a: number, b: number) => {
+		if (typeof a !== 'number' || typeof b !== 'number') return 0;
 		if (a === null) return 1;
 		if (b === null) return -1;
 		return a - b;
 	},
 	object: (a: object, b: object) => {
+		if (typeof a !== 'object' || typeof b !== 'object') return 0;
 		if (a === null) return 1;
 		if (b === null) return -1;
 		return 0;
 	},
 	string: (a: string, b: string) => {
+		if (typeof a !== 'string' || typeof b !== 'string') return 0;
 		if (a === null) return 1;
 		if (b === null) return -1;
 		return a.localeCompare(b);
 	},
 	symbol: (a: symbol, b: symbol) => {
+		if (typeof a !== 'symbol' || typeof b !== 'symbol') return 0;
 		if (a === null) return 1;
 		if (b === null) return -1;
 		return 0;
 	},
 	undefined: (a: undefined, b: undefined) => {
+		if (typeof a !== 'undefined' || typeof b !== 'undefined') return 0;
 		if (a === null) return 1;
 		if (b === null) return -1;
 		return 0;
