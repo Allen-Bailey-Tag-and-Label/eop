fdd3feffa1886757fe29c7f08947bcc02b25e197
bobthered <bobmcaleavey@gmail.com>
2025-06-25 15:20:07 -0400

    feat: add Header component

---
+++ src/lib/components/Header/Header.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+	import { type Attachment } from 'svelte/attachments';
+	import { twMerge } from 'tailwind-merge';
+	import { attachmentFactory } from '$lib/attachments';
+	import { theme as themeStore } from '$lib/theme';
+
+	type Props = {
+		attachments?: Attachment[];
+		children?: Snippet;
+		class?: string;
+		style?: string;
+		variants?: string[];
+	};
+	let {
+		attachments = $bindable([]),
+		children,
+		class: className,
+		style,
+		variants = [],
+		...restProps
+	}: Props = $props();
+</script>
+
+<header
+	{...restProps}
+	{@attach attachmentFactory(attachments)}
+	class={twMerge(
+		$themeStore.Header.default,
+		...variants.map((variant: string) => $themeStore.Header[variant]),
+		className
+	)}
+	{style}
+>
+	{#if children}
+		{@render children()}
+	{/if}
+</header>
