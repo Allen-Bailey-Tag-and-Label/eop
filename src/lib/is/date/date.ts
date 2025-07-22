d025559c708e325b683dac482ebb96b0a7e286d7
Bob McAleavey <bobmcaleavey@gmail.com>
2025-07-22 12:43:36 -0400

    feat: add date

---
+++ src/lib/is/date/date.ts
@@ -0,0 +1,1 @@
+export const date = (date: any) => date instanceof Date && !isNaN(date.getTime());
--- src/lib/is/is.ts
+++ src/lib/is/is.ts
@@ -1,9 +1,13 @@
+import { date } from './date';
 import { object } from './object';
 
 class Is {
-    get object() {
-        return (value: unknown): boolean => object(value)
-    }
+	get date() {
+		return (value: unknown): boolean => date(value);
+	}
+	get object() {
+		return (value: unknown): boolean => object(value);
+	}
 }
 
 export const is = new Is();
