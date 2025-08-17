ecdabf0bbfb62e3df7af2319543f40b3f75bef67
Bob McAleavey <bobmcaleavey@gmail.com>
2025-08-17 11:59:38 -0400

    fix: (padded) group padding

--- src/routes/(private)/(padded)/+layout.svelte
+++ src/routes/(private)/(padded)/+layout.svelte
@@ -4,6 +4,8 @@
 	let { children } = $props();
 </script>
 
-<Div class="flex flex-col px-4 pt-4 pb-[calc(1rem_+_env(safe-area-inset-bottom))]">
+<Div
+	class="flex flex-col px-4 pt-[calc(1rem_+_env(safe-area-inset-top))] pb-4 lg:pt-4 lg:pb-[calc(1rem_+_env(safe-area-inset-bottom))]"
+>
 	{@render children()}
 </Div>
