ab6a70c56b703744def11565907a54b47f4e6d2f
Bob McAleavey <bobmcaleavey@gmail.com>
2025-08-20 12:14:47 -0400

    fix: remove test from /dashboard

--- src/routes/(private)/(padded)/dashboard/+page.svelte
+++ src/routes/(private)/(padded)/dashboard/+page.svelte
@@ -6,7 +6,7 @@
 
 <Div class="flex flex-col items-start space-y-6">
 	<H1>Welcome {data.locals.user.profile.firstName}!</H1>
-	<Div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+	<!-- <Div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
 		<Card>Test</Card>
-	</Div>
+	</Div> -->
 </Div>
