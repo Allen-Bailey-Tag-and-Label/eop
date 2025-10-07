bdbcd34af8551c19c8465608b87801cd45c56ba3
Bob McAleavey <bobmcaleavey@gmail.com>
2025-10-07 09:38:51 -0400

    feat: add options

--- src/lib/formats/currency.ts
+++ src/lib/formats/currency.ts
@@ -1,7 +1,12 @@
-export const currency = (value:number) => {
-    const { format } = new Intl.NumberFormat('en-US', {
-        currency:'USD',
-        style:'currency',
-    });
-    return format( value );
-}
+type Options = {
+	minimumFractionDigits?: number;
+};
+
+export const currency = (value: number, options: Options = { minimumFractionDigits: 2 }) => {
+	const { format } = new Intl.NumberFormat('en-US', {
+		currency: 'USD',
+		minimumFractionDigits: options?.minimumFractionDigits ?? 2,
+		style: 'currency'
+	});
+	return format(value);
+};
