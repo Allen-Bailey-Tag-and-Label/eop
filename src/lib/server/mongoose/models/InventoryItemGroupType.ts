579f8f0ef050f32dc2d73542b7a491ba72a78cda
Bob McAleavey <bobmcaleavey@gmail.com>
2025-11-06 15:07:08 -0500

    feat: add /admin/item/group-types route

--- src/lib/server/mongoose/models/InventoryItemGroupType.ts
+++ src/lib/server/mongoose/models/InventoryItemGroupType.ts
@@ -1,5 +1,6 @@
 import { defineModel } from '../defineModel';
 
 export const InventoryItemGroupType = defineModel('InventoryItemGroupType', {
-	description: { type: String, required: true, unique: true }
+	code: { type: String, required: true, unique: true },
+	description: { type: String, required: true }
 });
