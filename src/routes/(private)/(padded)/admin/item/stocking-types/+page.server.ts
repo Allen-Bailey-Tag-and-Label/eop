74658221657b20cc044649069f275366115bd1c6
Bob McAleavey <bobmcaleavey@gmail.com>
2025-11-06 14:56:05 -0500

    feat: add /admin/item/stocking-types route

---
+++ src/routes/(private)/(padded)/admin/item/stocking-types/+page.server.ts
@@ -0,0 +1,14 @@
+import { InventoryItemStockingType } from '$lib/server/mongoose/models';
+import { serverLoad } from '$lib/server/mongoose/serverLoad';
+
+export const load = serverLoad({
+	labelFunctionMap: new Map([
+		[
+			'User',
+			(doc) => {
+				return doc.username;
+			}
+		]
+	]),
+	model: InventoryItemStockingType
+});
---
+++ src/routes/(private)/(padded)/admin/item/stocking-types/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { MongooseTable } from '$lib/components';
+
+	let { data } = $props();
+</script>
+
+<MongooseTable {data} isImportable={true} modelName={'InventoryItemStockingType'} />
